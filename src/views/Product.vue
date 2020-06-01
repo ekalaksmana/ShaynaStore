8b9051479ecb19e289eb2162f7ccbf1b63c59780
ekalaksmana <gedeekaputra31@gmail.com>
2020-06-02 01:08:42 +0800

    Bagian 3, video 8: menambahkan cart page

--- src/views/Product.vue
+++ src/views/Product.vue
@@ -87,7 +87,7 @@
                     <h4>$495.00</h4>
                   </div>
                   <div class="quantity">
-                    <a href="shopping-cart.html" class="primary-btn pd-cart">Add To Cart</a>
+                    <router-link to="/cart" class="primary-btn pd-cart">Add To Cart</router-link>
                   </div>
                 </div>
               </div>
@@ -98,120 +98,7 @@
     </section>
     <!-- Product Shop Section End -->
 
-    <!-- Related Products Section End -->
-    <div class="related-products spad">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12">
-            <div class="section-title">
-              <h2>Related Products</h2>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-lg-3 col-sm-6">
-            <div class="product-item">
-              <div class="pi-pic">
-                <img src="img/products/women-1.jpg" alt />
-                <ul>
-                  <li class="w-icon active">
-                    <a href="#">
-                      <i class="icon_bag_alt"></i>
-                    </a>
-                  </li>
-                  <li class="quick-view">
-                    <a href="#">+ Quick View</a>
-                  </li>
-                </ul>
-              </div>
-              <div class="pi-text">
-                <div class="catagory-name">Coat</div>
-                <a href="#">
-                  <h5>Pure Pineapple</h5>
-                </a>
-                <div class="product-price">
-                  $14.00
-                  <span>$35.00</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-3 col-sm-6">
-            <div class="product-item">
-              <div class="pi-pic">
-                <img src="img/products/women-2.jpg" alt />
-                <ul>
-                  <li class="w-icon active">
-                    <a href="#">
-                      <i class="icon_bag_alt"></i>
-                    </a>
-                  </li>
-                  <li class="quick-view">
-                    <a href="#">+ Quick View</a>
-                  </li>
-                </ul>
-              </div>
-              <div class="pi-text">
-                <div class="catagory-name">Shoes</div>
-                <a href="#">
-                  <h5>Guangzhou sweater</h5>
-                </a>
-                <div class="product-price">$13.00</div>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-3 col-sm-6">
-            <div class="product-item">
-              <div class="pi-pic">
-                <img src="img/products/women-3.jpg" alt />
-                <ul>
-                  <li class="w-icon active">
-                    <a href="#">
-                      <i class="icon_bag_alt"></i>
-                    </a>
-                  </li>
-                  <li class="quick-view">
-                    <a href="#">+ Quick View</a>
-                  </li>
-                </ul>
-              </div>
-              <div class="pi-text">
-                <div class="catagory-name">Towel</div>
-                <a href="#">
-                  <h5>Pure Pineapple</h5>
-                </a>
-                <div class="product-price">$34.00</div>
-              </div>
-            </div>
-          </div>
-          <div class="col-lg-3 col-sm-6">
-            <div class="product-item">
-              <div class="pi-pic">
-                <img src="img/products/women-4.jpg" alt />
-                <ul>
-                  <li class="w-icon active">
-                    <a href="#">
-                      <i class="icon_bag_alt"></i>
-                    </a>
-                  </li>
-                  <li class="quick-view">
-                    <a href="#">+ Quick View</a>
-                  </li>
-                </ul>
-              </div>
-              <div class="pi-text">
-                <div class="catagory-name">Towel</div>
-                <a href="#">
-                  <h5>Converse Shoes</h5>
-                </a>
-                <div class="product-price">$34.00</div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- Related Products Section End -->
+    <RelatedProduct />
 
     <FooterShayna />
   </div>
@@ -222,6 +109,7 @@
 // import HelloWorld from '@/components/HelloWorld.vue'
 import HeaderShayna from '@/components/HeaderShayna.vue';
 import FooterShayna from '@/components/FooterShayna.vue';
+import RelatedProduct from '@/components/RelatedProduct.vue';
 
 // Owl carousel Depen
 import carousel from 'vue-owl-carousel';
@@ -233,6 +121,7 @@ export default {
   components: {
     HeaderShayna,
     FooterShayna,
+    RelatedProduct,
     carousel
   },
   data() {
